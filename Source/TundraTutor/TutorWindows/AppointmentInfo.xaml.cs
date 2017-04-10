d345675975b913fe56c2d56ba587315ab496f2a9
Gabe Newell <caertl@coe.edu>
2017-04-10 12:38:55 -0500

    more Updates

--- Source/TundraTutor/TutorWindows/AppointmentInfo.xaml.cs
+++ Source/TundraTutor/TutorWindows/AppointmentInfo.xaml.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
 
-namespace TutorWindows
-{
-    /// <summary>
-    /// Interaction logic for AppointmentInfo.xaml
-    /// </summary>
-    public partial class AppointmentInfo : TundraControls.CustomWindow
-    {
-        public AppointmentInfo()
-        {
-            InitializeComponent();
-        }
-    }
-}
+//namespace TutorWindows
+//{
+//    /// <summary>
+//    /// Interaction logic for AppointmentInfo.xaml
+//    /// </summary>
+//    public partial class AppointmentInfo : TundraControls.CustomWindow
+//    {
+//        public AppointmentInfo()
+//        {
+//            InitializeComponent();
+//        }
+//    }
+//}
