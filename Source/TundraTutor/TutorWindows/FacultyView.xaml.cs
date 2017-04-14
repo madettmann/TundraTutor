39e532e8e2cf721cbc39b7f03fb4f43b2307b0e9
Gabe Newell <caertl@coe.edu>
2017-04-14 13:08:02 -0500

    More Updates

--- Source/TundraTutor/TutorWindows/FacultyView.xaml.cs
+++ Source/TundraTutor/TutorWindows/FacultyView.xaml.cs
@@ -58,6 +58,7 @@ namespace TutorWindows
                             select (i.Faculty.First_Name);
             var courseNames = from i in db.FacultyCourses.Local
                               select (i.Cours.CourseName);
+            string 
             facultyCoursViewSource.Source = "Name: " + facultyNames.ElementAt(0) + "\n Course: " + courseNames.ElementAt(0);
         }
         private void CustomWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
